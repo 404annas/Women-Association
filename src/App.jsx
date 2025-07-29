227593df3e933347284685e57cd2feb31b908052
Muhammad Annas Khan <annasking601@gmail.com>
2025-07-29 13:37:17 +0500

    Design Changes 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import Hero from './components/Hero';
-import About from './components/About';
-import What2 from './components/What2';
-import Events from './components/Events';
-import Partners from './components/Partners';
-import Contact from './components/Contact';
-import AboutAno from './components/AboutAno';
-import EventAno from './components/EventAno';
-import Donate from './components/Donate';
-import Save from './components/Save';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import What2 from "./components/What2";
+import Events from "./components/Events";
+import Partners from "./components/Partners";
+import Contact from "./components/Contact";
+import AboutAno from "./components/AboutAno";
+import EventAno from "./components/EventAno";
+import Donate from "./components/Donate";
+import Save from "./components/Save";
 
 const Home = () => (
   <>
@@ -26,11 +26,11 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='about' element={<AboutAno />} />
-        <Route path='events' element={<EventAno />} />
-        <Route path='donate' element={<Donate />} />
-        <Route path='save-heaven' element={<Save />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<AboutAno />} />
+        <Route path="events" element={<EventAno />} />
+        <Route path="donate" element={<Donate />} />
+        <Route path="save-heaven" element={<Save />} />
       </Route>
     </Routes>
   );
